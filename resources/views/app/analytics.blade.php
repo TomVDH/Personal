541066dca8d9ae9e32ff8fa4a430324536d8ef15
TomVDH <tom.vanderheyden01@gmail.com>
2020-03-27 04:18:23 -0700

    Added Google Analytics

---
+++ resources/views/app/analytics.blade.php
@@ -0,0 +1,10 @@
+@yield('layout')
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162025720-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-162025720-1');
+    console.log('GA ON')
+</script>
